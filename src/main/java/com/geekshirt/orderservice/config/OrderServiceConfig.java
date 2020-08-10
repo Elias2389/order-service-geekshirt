2e2f4170283d3862e31e25c11181b956964111b4
Andres <andres.rivas@mercadolibre.com>
2020-08-10 17:19:27 -0300

    Add converter

--- src/main/java/com/geekshirt/orderservice/config/OrderServiceConfig.java
+++ src/main/java/com/geekshirt/orderservice/config/OrderServiceConfig.java
@@ -1,7 +1,13 @@
 package com.geekshirt.orderservice.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OrderServiceConfig {
+    @Bean
+    public ModelMapper provideModelMapper() {
+        return new ModelMapper();
+    }
 }
